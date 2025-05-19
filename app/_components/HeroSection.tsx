0cf05806839c429c792b443ea153c320bacf9b81
Utkarsh <seutkarsh@gmail.com>
2025-05-19 11:40:40 +0400

    Build Changes

--- app/_components/HeroSection.tsx
+++ app/_components/HeroSection.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
 import Link from "next/link";
